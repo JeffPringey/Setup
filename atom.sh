5e8b19a1d2f78e2ab718aa5f00bb3f877349b742
Jeff Pringey <jpringey@jpringey-IT.local>
2015-07-04 20:31:25 -0500

    Initial Commit

---
+++ atom.sh
@@ -0,0 +1,6 @@
+apm install language-tcl
+apm install linter
+apm install linter-js-yaml
+apm install merge-conflicts
+apm install minimap
+apm install vim-mode
---
+++ brew.sh
@@ -0,0 +1,22 @@
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+brew install mtr
+brew install ansible
+brew install cellar
+brew cask install
+brew cask install caffeine
+brew cask install Asepsis
+brew cask install KeePassX
+brew cask install virtualbox
+brew cask install vagrant
+brew cask install atom
+brew cask install hipchat
+brew install pass
+brew install gpg
+brew install heroku-toolbelt
+brew cask install sequel-pro
+brew cask install handbrake
+brew cask install transmission
+brew cask install plex-home-theatre
+brew cask install dropbox
+brew cask install forklift
+brew cleanup
